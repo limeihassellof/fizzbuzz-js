e775024c6c05ff94121a8a367317c27b2d057151
limei <limei@evolve.se>
2018-04-22 21:50:29 +0200

    the solution

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,6 +1,18 @@
+
+
 const FizzBuzz = {
   eval(number) {
-    // Write your code here
+    if (typeof number === 'string') {
+      return "Error";
+    } else if (number % 15 == 0) {
+      return 'FizzBuzz';
+    } else if (number % 5 == 0) {
+      return 'Buzz';
+    } else if (number % 3 == 0) {
+      return 'Fizz';
+    } else {
+      return number;
+    }
   }
 };
 
